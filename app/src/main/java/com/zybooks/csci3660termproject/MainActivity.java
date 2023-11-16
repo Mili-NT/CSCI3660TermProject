562988fa02d06f5ff2892067473053169008ac93
Mili <mili@mili.mil>
2023-11-16 15:14:12 -0500

    Created the retrofit client and api interface instance in MainActivity.java

--- app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
+++ app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
@@ -1,17 +1,23 @@
 package com.zybooks.csci3660termproject;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import com.zybooks.csci3660termproject.api.WordAPIManager;
+import com.zybooks.csci3660termproject.retrofit.WordAPI;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private WordAPI wordAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create Retrofit client
+        Retrofit retrofit = WordAPIManager.getClient();
+        // Create API interface
+        wordAPI = retrofit.create(WordAPI.class);
     }
-
-
-
-    }
+}
