213b4f0a9a474567cf45e01aaaa5f869de4b59fe
Mili <mili@mili.mil>
2023-11-16 14:19:20 -0500

    Created api package and moved WordAPI into it

---
+++ app/src/main/java/com/zybooks/csci3660termproject/api/WordAPI.java
@@ -0,0 +1,9 @@
+package com.zybooks.csci3660termproject.api;
+
+
+public class WordAPI {
+    private static final String X_RAPIDAPI_KEY = "";
+    private static final String X_RAPIDAPI_HOST ="wordsapiv1.p.rapidapi.com";
+    public WordAPI() {
+    }
+}
