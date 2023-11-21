b4445a96f5d94102fd4f789c1c84ce98b1fefdef
OpportuneMoney <tipofthehat789@gmail.com>
2023-11-21 15:01:48 -0500

    Added a bottom nav bar. It seems to be set up correctly but the UI does not run for me. Also created an image for the bottom nav bar.

--- app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
+++ app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
@@ -1,15 +1,24 @@
 package com.zybooks.csci3660termproject;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.zybooks.csci3660termproject.api.WordAPIManager;
 import com.zybooks.csci3660termproject.retrofit.WordAPI;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private WordAPI wordAPI;
+    private BottomNavigationView bottomNav;
+    private NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = WordAPIManager.getClient();
         // Create API interface
         wordAPI = retrofit.create(WordAPI.class);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        navController = Navigation.findNavController(this, R.id.fragment_container);
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
