3bdd13967dcaac696c51a661a01f28927d7f4a25
Mili <mili@mili.mil>
2023-11-21 16:08:00 -0500

    Removed nav_graph.xml, bottom_nav_menu.xml, and refactored activity_main.xml and MainActivity.java to be buildable

--- app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
+++ app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
@@ -1,8 +1,8 @@
 package com.zybooks.csci3660termproject;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.NavigationUI;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,8 +16,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private WordAPI wordAPI;
-    private BottomNavigationView bottomNav;
-    private NavController navController;
+    //private BottomNavigationView bottomNav;
+    //private NavController navController;
 
 
     @Override
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
         // Create API interface
         wordAPI = retrofit.create(WordAPI.class);
 
-        bottomNav = findViewById(R.id.bottom_nav);
-        navController = Navigation.findNavController(this, R.id.fragment_container);
+        //bottomNav = findViewById(R.id.bottom_nav);
+        //navController = Navigation.findNavController(this, R.id.fragment_container);
 
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        //NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
