4da03c251fa1dac7b6c208ef6ef019cefe3f7e08
bradymedlin <130109924+bradymedlin@users.noreply.github.com>
2023-12-07 08:11:31 -0500

    trying to add animations

--- app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
+++ app/src/main/java/com/zybooks/csci3660termproject/MainActivity.java
@@ -7,7 +7,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView navView;
@@ -18,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //nav bar
         navView = findViewById(R.id.nav_view);
         navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
@@ -26,6 +36,29 @@ public class MainActivity extends AppCompatActivity {
             NavController navController = navHostFragment.getNavController();
             NavigationUI.setupWithNavController(navView, navController);
         }
+
+        //starry background
+        FrameLayout starrySky = findViewById(R.id.starrySky);
+
+        for (int i = 0; i < 100; i++) {
+            ImageView star = new ImageView(this);
+            star.setImageResource(R.drawable.star_shape);
+
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.star_fade);
+            starrySky.startAnimation(animation);
+
+            starrySky.addView(star);
+
+            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) star.getLayoutParams();
+            params.leftMargin = getRandomPosition();
+            params.topMargin = getRandomPosition();
+            star.setLayoutParams(params);
+        }
     }
+
+    private int getRandomPosition() {
+        return (int) (Math.random() * getResources().getDisplayMetrics().widthPixels);
+    }
+
 }
 
